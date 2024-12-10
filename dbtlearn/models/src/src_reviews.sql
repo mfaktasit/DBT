94486ac68c8a1d8411a84b30087ccbb06b0af9a3
Fatih <mfaktasit@gmail.com>
2024-12-10 23:22:20 +0100

    Dbt files uploaded

---
+++ dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,14 @@
+WITH raw_rewiews AS (
+    SELECT
+        *
+    FROM
+        {{ source('airbnb', 'reviews') }}
+)
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment,
+FROM
+    raw_rewiews
